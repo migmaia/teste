17678f6d38139a05607af62b6ce993ff4a80aad9
migmaia <79366501+migmaia@users.noreply.github.com>
2023-10-31 22:49:28 +0000

    Add files via upload

---
+++ pagina-nao.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Página não</title>
+</head>
+<body>
+  <h1>Página não</h1>
+
+  <p>Você respondeu "Não".</p>
+
+  <p>Aqui estão algumas instruções:</p>
+
+  <p>...</p>
+</body>
+</html>
---
+++ pagina-sim.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Página sim</title>
+</head>
+<body>
+  <h1>Página sim</h1>
+
+  <p>Você respondeu "Sim".</p>
+
+  <p>Aqui está algumas informações adicionais:</p>
+
+  <p>...</p>
+</body>
+</html>
